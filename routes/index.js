4810a2d6ed56a0929ba18943d31dedfa47a229a4
Dallas <dallas@w134-87-146-076.wireless.uvic.ca>
2017-02-10 16:18:10 -0800

    Added AI Logic

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,15 @@ var config  = require('../config.json');
 var express = require('express');
 var router  = express.Router();
 
+var gameWidth = 0;
+var gameHeight = 0;
+var gameID = '';
+//var board = [];
+var foods = [];
+var snakes = [];
+var turn = 0;
+var you = '';
+
 // Handle GET request to '/'
 router.get(config.routes.info, function (req, res) {
   // Response data
@@ -13,23 +22,78 @@ router.get(config.routes.info, function (req, res) {
   return res.json(data);
 });
 
+
+//START START START
 // Handle POST request to '/start'
 router.post(config.routes.start, function (req, res) {
   // Do something here to start the game
 
+
+  var input = req.body;
+
+  gameWidth = input.width;
+  gameHeight = input.height;
+  gameID = input.game_id;
   // Response data
   var data = {
-    taunt: config.snake.taunt.start
+    color: config.snake.color,
+    head_url: config.snake.head_url,    
+    name: 'Bowser',
+    taunt: config.snake.taunt.start,
+
   };
 
   return res.json(data);
 });
 
+
+//MOVE MOVE MOVE
 // Handle POST request to '/move'
 router.post(config.routes.move, function (req, res) {
   // Do something here to generate your move
-
   // Response data
+  var input = req.body;
+
+
+  gameWidth = input.width;
+  gameHeight = input.height;
+  gameID = input.game_id;
+  snakes = input.snakes;
+  turn = input.turn;
+  foods = input.food;
+  you = input.you; 
+  var health;
+  var position;
+
+  var enemySnakeHeads;
+  var dangerousPositions;
+
+  //build my snake, and track enemy snakes
+  for(var i = 0; i < snakes.length; i++)
+  {
+    if (snakes[i].id == you)
+    {
+      health = snakes[i].health_points;
+      for(var j = 0; j < snakes[i].coords.length)
+      {
+        position.push(snakes[i].coords[j]);
+      }
+    }
+    else
+    {
+      enemySnakeHeads.push(snakes[i]coords[0]);
+      for(int j = 0; j < snakes[i].coords.length)
+      {
+        dangerousPositions.push(snakes[i].coords[j]);
+      }
+    }
+
+  }
+
+//  var headPosition =
+
+
+
   var data = {
     move: 'north', // one of: ["north", "east", "south", "west"]
     taunt: config.snake.taunt.move
