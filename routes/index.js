cc4a15523144045f7415fa68b8b42a96c9d6647f
Dallas <dallas@w134-87-151-189.wireless.uvic.ca>
2017-03-01 15:59:49 -0800

    New node-js base

--- routes/index.js
+++ routes/index.js
@@ -1,58 +1,26 @@
-var config  = require('../config.json');
-var express = require('express');
-var router  = express.Router();
+var express = require('express')
+var router  = express.Router()
 
-var gameWidth = 0;
-var gameHeight = 0;
-var gameID = '';
-//var board = [];
-var foods = [];
-var snakes = [];
-var turn = 0;
-var you = '';
-
-// Handle GET request to '/'
-router.get(config.routes.info, function (req, res) {
-  // Response data
-  var data = {
-    color: config.snake.color,
-    head_url: config.snake.head_url,
-  };
-
-  return res.json(data);
-});
-
-
-//START START START
 // Handle POST request to '/start'
-router.post(config.routes.start, function (req, res) {
-  // Do something here to start the game
+router.post('/start', function (req, res) {
+  // NOTE: Do something here to start the game
 
-
-  var input = req.body;
-
-  gameWidth = input.width;
-  gameHeight = input.height;
-  gameID = input.game_id;
   // Response data
   var data = {
-    color: config.snake.color,
-    head_url: config.snake.head_url,    
-    name: 'Bowser',
-    taunt: config.snake.taunt.start,
+    color: "#008080",
+    name: "Bowser",
+    head_url: "http://bsnek.herokuapp.com/", // optional, but encouraged!
+    taunt: "Let's do thisss thang!", // optional, but encouraged!
+  }
 
-  };
+  return res.json(data)
+})
 
-  return res.json(data);
-});
-
-
-//MOVE MOVE MOVE
 // Handle POST request to '/move'
-router.post(config.routes.move, function (req, res) {
-  // Do something here to generate your move
-  // Response data
-  var input = req.body;
+router.post('/move', function (req, res) {
+  // NOTE: Do something here to generate your move
+
+var input = req.body;
 
 
   gameWidth = input.width;
@@ -96,23 +64,14 @@ var j;
     }
   }
 
+
+  // Response data
   var data = {
-    move: 'south', // one of: ["north", "east", "south", "west"]
-    taunt: config.snake.taunt.move
-  };
+    move: 'down', // one of: ['up','down','left','right']
+    taunt: 'Outta my way, snake!', // optional, but encouraged!
+  }
 
-  return res.json(data);
-});
+  return res.json(data)
+})
 
-// Handle POST request to '/end'
-router.post(config.routes.end, function (req, res) {
-  // Do something here to end your snake's session
-
-  // We don't need a response so just send back a 200
-  res.status(200);
-  res.end();
-  return;
-});
-
-
-module.exports = router;
+module.exports = router
