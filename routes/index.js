b8f06f107a0245aeaac0d97107f5e05119e624ab
Dallas <dallas@w134-87-151-189.wireless.uvic.ca>
2017-03-01 18:15:11 -0800

    testing food chasing

--- routes/index.js
+++ routes/index.js
@@ -20,9 +20,75 @@ router.post('/start', function (req, res) {
 router.post('//move', function (req, res) {
   // NOTE: Do something here to generate your move
 
+  var input = req.body;
+
+
+  gameWidth = input.width;
+  gameHeight = input.height;
+  gameID = input.game_id;
+  snakes = input.snakes;
+  turn = input.turn;
+  foods = input.food;
+  you = input.you; 
+  var health;
+  var position;
+
+  var enemySnakeHeads;
+  var dangerousPositions;
+  var headPosition;
+
+
+
+//build my snake, and track enemy snakes 
+var i;
+var j;
+
+ for(i = 0; i < snakes.length; i++)
+ {
+    if (snakes[i].id == you)
+    {
+      health = snakes[i].health_points;
+      headPosition = snakes[i].coords[0];
+      for(j = 0; j < snakes[i].coords.length; j++)
+      {
+        position.push(snakes[i].coords[j]);
+      }
+    }
+    else
+    {
+      enemySnakeHeads.push(snakes[i].coords[0]);
+      for(j = 0; j < snakes[i].coords.length; j++)
+      {
+        dangerousPositions.push(snakes[i].coords[j]);
+      }
+    }
+  }
+
+  var finalMove = 'left';
+
+  if (foods[0][0] > headPosition[0])
+  {
+    finalMove = 'right';
+  }
+
+  if (foods[0][0] < headPosition[0])
+  {
+    finalMove = 'left';
+  }
+
+  if (foods[0][1] < headPosition[1])
+  {
+    finalMove = 'down';
+  }
+  if (foods[0][1] > headPosition[1])
+  {
+    finalMove = 'up';
+  }
+
+
   // Response data
   var data = {
-    move: 'down', // one of: ['up','down','left','right']
+    move: finalMove, // one of: ['up','down','left','right']
     taunt: 'Outta my way, snake!', // optional, but encouraged!
   }
 
