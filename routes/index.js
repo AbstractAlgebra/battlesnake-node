d2f0b037ee623b449e3fab9ca7abdc34da11d304
Dallas <dallas@w134-87-151-189.wireless.uvic.ca>
2017-03-01 20:05:07 -0800

    debugging

--- routes/index.js
+++ routes/index.js
@@ -60,6 +60,7 @@ for(i = 0; i < snakes.length; i++)
     headPosition = snakes[i].coords[0];
     for(j = 0; j < snakes[i].coords.length; j++)
     {
+      dangerousPositions.push(snakes[i].coords[j]);
       position.push(snakes[i].coords[j]);
     }
 
@@ -75,28 +76,114 @@ for(i = 0; i < snakes.length; i++)
 }
  // }
 
-
+var leftCount = 0;
+var rightCount = 0;
+var upCount = 0;
+var downCount = 0;
 
   var finalMove = 'left';
 
   if (foods[0][0] > headPosition[0])
   {
+    rightCount += 2;
     finalMove = 'right';
   }
 
   if (foods[0][0] < headPosition[0])
   {
+    leftCount += 2;
     finalMove = 'left';
   }
 
   if (foods[0][1] > headPosition[1])
   {
+    downCount += 2;
     finalMove = 'down';
   }
   if (foods[0][1] < headPosition[1])
   {
+    upCount += 2;
     finalMove = 'up';
   }
+
+
+  //avoid deadly positions
+  for(int i = 0; i < dangerousPositions.length; i++)
+  {
+    var headX = headPosition[0];
+    var headY = headPosition[1];
+
+    dangerX = dangerousPositions[i][0];
+    dangerY = dangerousPositions[i][1];
+    //if one right is dangerous
+    if( ((headX + 1) ==  dangerX)  & headY == dangerY)
+    {
+      rightCount = 0;
+    }
+ 
+
+    if( ((headX - 1) ==  dangerX)  & headY == dangerY)
+    {
+      leftCount = 0;
+    } 
+
+    if( headX  ==  dangerX  & ( headY + 1) == dangerY)
+    {
+      downCount = 0;
+    } 
+
+    if( headX ==  dangerX  & (headY - 1) == dangerY)
+    {
+      upCount = 0;
+    } 
+
+  }
+
+  //handle avoiding walls
+  if(headPosition[0] == 0)
+  {
+    leftCount = 0;
+  }
+
+  if(headPosition[0] == width)
+  {
+    rightCount = 0;
+  }
+
+  if(headPosition[1] == 0)
+  {
+    upCount = 0;
+  }
+
+  if(headPosition[1] == height)
+  {
+    downCount = 0;
+  }
+
+  //pick safest direction
+
+  if (downCount > leftCount && downCount > rightCount && downCount > upCount)
+  {
+    finalMove = 'down';
+  }
+
+  if (upCount > leftCount && upCount > rightCount && upCount > downCount)
+  {
+    finalMove = 'up';
+  }
+
+  if (leftCount > rightCount && leftCount > downCount && leftCount > upCount)
+  {
+    finalMove = 'left';
+  }
+
+  if (rightCount > leftCount && rightCount > downCount && rightCount > upCount)
+  {
+    finalMove = 'right';
+  }
+
+
+
   var data = {
     move: finalMove, // one of: ['up','down','left','right']
     taunt: 'Outta my way, snake!', // optional, but encouraged!
