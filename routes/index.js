4a7e0d35607374cb38f0d61c00f9dc29c27bc6b2
rooooooooob <raftias@gmail.com>
2017-03-04 14:18:15 -0800

    :|

--- routes/index.js
+++ routes/index.js
@@ -17,13 +17,86 @@ router.post('/start', function (req, res) {
   return res.json(data)
 })
 
+function TinyQueue(data, compare) {
+    if (!(this instanceof TinyQueue)) return new TinyQueue(data, compare);
 
+    this.data = data || [];
+    this.length = this.data.length;
+    this.compare = compare || defaultCompare;
+
+    if (data) for (var i = Math.floor(this.length / 2); i >= 0; i--) this._down(i);
+}
+
+function defaultCompare(a, b) {
+    return a < b ? -1 : a > b ? 1 : 0;
+}
+
+TinyQueue.prototype = {
+
+    push: function (item) {
+        this.data.push(item);
+        this.length++;
+        this._up(this.length - 1);
+    },
+
+    pop: function () {
+        var top = this.data[0];
+        this.data[0] = this.data[this.length - 1];
+        this.length--;
+        this.data.pop();
+        this._down(0);
+        return top;
+    },
+
+    peek: function () {
+        return this.data[0];
+    },
+
+    _up: function (pos) {
+        var data = this.data,
+            compare = this.compare;
+
+        while (pos > 0) {
+            var parent = Math.floor((pos - 1) / 2);
+            if (compare(data[pos], data[parent]) < 0) {
+                swap(data, parent, pos);
+                pos = parent;
+
+            } else break;
+        }
+    },
+
+    _down: function (pos) {
+        var data = this.data,
+            compare = this.compare,
+            len = this.length;
+
+        while (true) {
+            var left = 2 * pos + 1,
+                right = left + 1,
+                min = pos;
+
+            if (left < len && compare(data[left], data[min]) < 0) min = left;
+            if (right < len && compare(data[right], data[min]) < 0) min = right;
+
+            if (min === pos) return;
+
+            swap(data, min, pos);
+            pos = min;
+        }
+    }
+};
+
+function swap(data, i, j) {
+    var tmp = data[i];
+    data[i] = data[j];
+    data[j] = tmp;
+}
 
 // Handle POST request to '/move'
 router.post('//move', function (req, res) {
-
-
 	// NOTE: Do something here to generate your move
+	
 	var input = req.body;
 	var gameWidth = input.width;
 	var gameHeight = input.height;
@@ -38,14 +111,6 @@ router.post('//move', function (req, res) {
 	var enemySnakeHeads = new Array();
 	var dangerousPositions = new Array();
 	var headPosition;
-
-
-
-	//build my snake, and track enemy snakes 
-	//var k = snakes.length;
-	var k = snakes.length;
-
-	// {
 
 	for(i = 0; i < snakes.length; i++)
 	{
@@ -68,32 +133,23 @@ router.post('//move', function (req, res) {
 			}
 		}
 	}
-
-
-	// }
 	
+	const hx = headPosition[0];
+	const hy = headPosition[1];
+	const NONE = -1;
+	const DOWN = 0;
+	const RIGHT = 1;
+	const LEFT = 2;
+	const UP = 3;
+	const offsets = [[0, 1], [1, 0], [-1, 0], [0, -1]];
+	const reverseMove = [3, 2, 1, 0];
 	
-	var hx = headPosition[0];
-	var hy = headPosition[1];
-	var NONE = -1;
-	var DOWN = 0;
-	var RIGHT = 1;
-	var LEFT = 2;
-	var UP = 3;
-	var offsets = [[0, 1], [1, 0], [-1, 0], [0, -1]];
-	var reverseMove = [3, 2, 1, 0];
-	
-	var queue = new PriorityQueue({
-		comparator: function(a, b) {
-
-			return weight[b[0]][b[1]] - weight[a[0]][a[1]];
-		}
-	});
 	var visited = new Array(gameWidth);
 	var weight = new Array(gameWidth);
 	var prev = new Array(gameWidth);
 	var plen = new Array(gameWidth);
 	var foodAtArr = new Array(gameWidth);
+	var snakeWeight = new Array(gameWidth);
 	for (var i = 0; i < gameWidth; ++i)
 	{
 		visited[i] = new Array(gameHeight);
@@ -101,6 +157,7 @@ router.post('//move', function (req, res) {
 		prev[i] = new Array(gameHeight);
 		plen[i] = new Array(gameHeight);
 		foodAtArr[i] = new Array(gameHeight);
+		snakeWeight[i] = new Array(gameHeight);
 		for (var j = 0; j < gameHeight; ++j)
 		{
 			visited[i][j] = false;
@@ -108,41 +165,67 @@ router.post('//move', function (req, res) {
 			prev[i][j] = NONE;
 			plen[i][j] = 0;
 			foodAtArr[i][j] = false;
+			snakeWeight[i][j] = 0;
 		}
 	}
-	 var backupPlan = {
-	 	move: 'left', // one of: ['up','down','left','right']
-	 	taunt: 'Zoom zoom!', // ofptional, but encouraged!
-	 }
-
-
-
-	 return res.json(backupPlan)
-
-	/*for (var i = 0; i < dangerousPositions.length; ++i)
+	for (var i = 0; i < dangerousPositions.length; ++i)
 	{
-		visited[dangerousPositions[i][0], dangerousPositions[i][1]] = true;
+		const dx = dangerousPositions[i][0];
+		const dy = dangerousPositions[i][1];
+		visited[dx][dy] = true;
+		for (var j = -1; j < 2; ++j)
+		{
+			for (var k = -1; k < 2; ++k)
+			{
+				const dpx = dx + j;
+				const dpy = dy + k;
+				if (dpx >= 0 && dpx < gameWidth && dpy >= 0 && dpy < gameHeight)
+				{
+					snakeWeight[dpx][dpy] += 1;
+				}
+			}
+		}
 	}
 	for (var i = 0; i < foods.length; ++i)
 	{
 		foodAt[foods[i][0]][foods[i][1]] = true;
 	}
-	var foodAt = function(fx, fy)
+	function foodAt(fx, fy)
 	{
 		return foodAtArr[fx][fy];
 	}
-	// @TODO: keep track of distance to closest food
-	//        maybe if hp < 40 || hp < dist * 2, go for it?
-	queue.queue([hx, hy]);
+	function heurisic(hx, hy)
+	{
+		var foodDist = 0;
+		for (var i = 0; i < foods.length; ++i)
+		{
+			foodDist += mdist(hx, hy, foods[i][0], foods[i][1]);
+		}
+		return 1 + snakeWeight[hx][hy] + foodDist * 0.1 / (0.1 + foods.length);
+	}
+	function mdist(ax, ay, bx, by)
+	{
+		var xd = ax - by;
+		var yd = ay - by;
+		if (xd < 0) xd = -xd;
+		if (yd < 0) yd = -yd;
+		return xd + yd;
+	}
+	var queue = new TinyQueue({
+		compare: function(a, b) {
+			return weight[b[0]][b[1]] - weight[a[0]][a[1]];
+		}
+	});
+	queue.push([hx, hy]);
 	weight[hx][hy] = 0;
 	var targetX = -1;
 	var targetY = -1;
-	while (queue.length() > 0)
+	while (queue.length > 0)
 	{
-		var cur = queue.dequeue(); // current node to explore
-		var cx = cur[0];
-		var cy = cur[1];
-		var cw = weight[cx][cy];// current weight
+		const cur = queue.pop(); // current node to explore
+		const cx = cur[0];
+		const cy = cur[1];
+		const cw = weight[cx][cy];// current weight
 		visited[cx][cy] = true;
 		// @TODO remove when we don't want to just move to nearest food
 		if (targetX == -1 || plen[cx][cy] > plen[targetX][targetY])
@@ -150,7 +233,7 @@ router.post('//move', function (req, res) {
 			targetX = cx;
 			targetY = cy;
 		}
-		if (foodAt(cx, cy))
+		if (foodAt(cx, cy) && (health < 40 || 2 * hp < plen[cx][cy]))
 		{
 			targetX = -1;
 			targetY = -1;
@@ -159,17 +242,18 @@ router.post('//move', function (req, res) {
 		for (var o = 0; o < 4; ++o)
 		{
 			// try all offsets (left, right, ec) and if not visited, move to
-			var px = o[0] + cx;
-			var py = o[1] + cy;
+			const px = o[0] + cx;
+			const py = o[1] + cy;
 			if (!visited[px][py] && px >= 0 && py >= 0 && px < gameWidth && py < gameHeight)
 			{
-				var pw = cw + 1; // @TODO: add in a weight heuristic
+				const pw = cw + heurisic(px, py);
 				if (pw < weight[px][py])
 				{
 					// update weight if we can get there cheaper
 					weight[px][py] = pw;
 					prev[px][py] = reverseMove[o];
-					queue.queue([px, py]);
+					plen[px][py] = plen[cx][cy] + 1;
+					queue.push([px, py]);
 				}
 			}
 		}
@@ -192,20 +276,182 @@ router.post('//move', function (req, res) {
 			tx = offsets[prev[tx][ty]][0];
 			ty = offsets[prev[tx][ty]][1];
 		}
-		var move = reverseMove[prev[ptx][pty]];
-		var resp = ['down', 'right', 'left', 'up'];
+		const move = reverseMove[prev[ptx][pty]];
+		const resp = ['down', 'right', 'left', 'up'];
 		var data = {
 			move: resp[move], // one of: ['up','down','left','right']
 			taunt: 'Zoom zoom!', // optional, but encouraged!
 		}
 
 		return res.json(data)
-	}*/
+	}
+
+	var leftCount = 0;
+	var rightCount = 0;
+	var upCount = 0;  
+	var downCount = 0;
 
 
+	//only seek out food if hungry
+	if( health < 50)
+	{
+		if (foods[0][0] > headPosition[0])
+		{
+			rightCount += 2;
+		} else 
+		{
+			rightCount += 1;
+		}
+
+		if (foods[0][0] < headPosition[0])
+		{
+			leftCount += 2;
+		}
+		else
+		{
+			leftCount += 1;
+		}
+
+		if (foods[0][1] > headPosition[1])
+		{
+			downCount += 2;
+		}
+		else 
+		{
+			downCount += 1;
+		}
+		if (foods[0][1] < headPosition[1])
+		{
+			upCount += 2;
+		} else
+		{
+			upCount += 1;
+		}
+	}
+	else
+	{
+		var low = 1;
+		var high = 4;
+		rightCount += (Math.floor(Math.random() * (high - low + 1) + low));
+		leftCount += (Math.floor(Math.random() * (high - low + 1) + low));
+		downCount += (Math.floor(Math.random() * (high - low + 1) + low));
+		upCount += Math.floor(Math.random() * (high - low + 1) + low);
+	}
+
+
+	//avoid deadly positions
+
+	for(i = 0; i < dangerousPositions.length; i++)
+	{
+
+		var headX = headPosition[0];
+		var headY = headPosition[1];
+
+		var dangerX = dangerousPositions[i][0];
+		var dangerY = dangerousPositions[i][1];
+		//if one right is dangerous
+
+		if( ((headX + 1) ==  dangerX)  & (headY == dangerY))
+		{
+			rightCount = -1;
+		}
+
+
+		if( ((headX - 1) ==  dangerX)  & (headY == dangerY))
+		{
+			leftCount = -1;
+		} 
+
+		if( (headX  ==  dangerX)  & (( headY + 1) == dangerY))
+		{
+			downCount = -1;
+		} 
+
+		if( (headX ==  dangerX)  & ((headY - 1) == dangerY))
+		{
+			upCount = -1;
+		} 
+
+	}
+
+	//handle avoiding walls
+	if(headPosition[0] == 0)
+	{
+		leftCount = -1;
+	}
+
+	if(headPosition[0] == gameWidth-1)
+	{
+		rightCount = -1;
+	}
+
+	if(headPosition[1] == 0)
+	{
+		upCount = -1;
+	}
+
+	if(headPosition[1] == gameHeight-1)
+	{
+		downCount = -1;
+	}
+
+	//default to left
+	var finalMove = 'left';
+
+	//pick safest direction
+
+	if (downCount >= leftCount && downCount >= rightCount && downCount >= upCount && downCount != -1)
+	{
+		finalMove = 'down';
+	}
+
+	if (upCount >= leftCount && upCount >= rightCount && upCount >= downCount && upCount != -1)
+	{
+		finalMove = 'up';
+	}
+
+	if (leftCount >= rightCount && leftCount >= downCount && leftCount >= upCount && leftCount != 1)
+	{ 
+		finalMove = 'left';
+	}
+
+	if (rightCount >= leftCount && rightCount >= downCount && rightCount >= upCount && rightCount != 1)
+	{
+		finalMove = 'right';
+	}
+
+	//handle when only one direction is safe
+	if (rightCount == -1 && leftCount == -1 && upCount == -1)
+	{
+		finalMove = 'down';
+	}
+
+	if (rightCount == -1 && leftCount == -1 && downCount == -1)
+	{
+		finalMove = 'up';
+	}
+
+	if (downCount == -1 && leftCount == -1 && upCount == -1)
+	{
+		finalMove = 'right';
+	}
+
+	if (rightCount == -1 && downCount == -1 && upCount == -1)
+	{
+		finalMove = 'left';
+	}
+
+
+	var data = {
+		move: finalMove, // one of: ['up','down','left','right']
+		taunt: 'Zoom zoom!', // optional, but encouraged!
+	}
+
+	return res.json(data)
+
+
+	// Response data
 
 })
-
-
 
 module.exports = router
